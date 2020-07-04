771e1976aa5f955adfa94c6e2d83551909645489
Shreya Galani <shreya.galani@theredpandas.com>
2020-07-04 15:53:54 +0530

    added readme file

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Parking Lot
+
+To run this Application make sure you have node installed on you machine
+
+To chech node version you can use the below command
+ $ `node -v`
+
+To install node you can follow the below link
+ `https://nodejs.org/en/download/`
+
+To test parking lot Application you need to run setup first
+
+To run setup you can use the below command
+ $ `bin/setup`
+
+To run the application against custom input you can run the below command from parking_lot directory
+ $ `node index.js <input_file>`
+ 
+To run the test cases use below command
+ $ `npm run test`
+
+To run functional test suite please refer the README.md included in `functional_specs` folder
+
+
+
+
--- index.js
+++ index.js
@@ -4,68 +4,70 @@ const maximumSlots = 100
 var totalParkingSlots
 
 function processInputCommand(command) {
-        const commandAndArguments = command.split(' ')
-        switch (commandAndArguments[0]) {
-            case 'create_parking_lot': {
-                if (commandAndArguments[1] > maximumSlots) {
-                    console.log(`Parking lot has maximum capacity of ${maximumSlots} slots`)
-                } else {
-                    totalParkingSlots = new Array(commandAndArguments[1])
-                    for (var i = 0; i < commandAndArguments[1]; i++) {
-                        totalParkingSlots[i] = null
-                    }
-                    console.log(`Created parking lot with ${commandAndArguments[1]} slots`)
-
-                }
-
-            }
-            break;
-        case 'park': {
-            const slotNumber = totalParkingSlots.indexOf(null)
-            if (slotNumber !== -1) {
-                totalParkingSlots[slotNumber] = commandAndArguments[1];
-                console.log(`Allocated slot number: ${slotNumber+1}`)
+    const commandAndArguments = command.split(' ')
+    switch (commandAndArguments[0]) {
+        case 'create_parking_lot': {
+            if (commandAndArguments[1] > maximumSlots) {
+                console.log(`Parking lot has maximum capacity of ${maximumSlots} slots`)
             } else {
-                console.log('Sorry, parking lot is full')
-            }
-        }
-        break;
-        case 'leave': {
-            const indexOfParkedCar = totalParkingSlots.indexOf(commandAndArguments[1])
-            if (indexOfParkedCar !== -1) {
-                const hours = commandAndArguments[2]
-                const charge = 10 + ((hours - 2) * 10)
-                console.log(`Registration number ${commandAndArguments[1]} with Slot Number ${indexOfParkedCar+1} is free with Charge ${charge}`);
-                totalParkingSlots[indexOfParkedCar] = null
-            } else {
-                console.log(`Registration number ${commandAndArguments[1]} not found`);
-            }
-        }
-        break;
-        case 'status': {
-            console.log(`Slot No.    Registration No.`)
-            totalParkingSlots.forEach((parkedCar, index) => {
-                if (parkedCar) {
-                    console.log(`${index+1}           ${parkedCar}`)
+                totalParkingSlots = new Array(commandAndArguments[1])
+                for (var i = 0; i < commandAndArguments[1]; i++) {
+                    totalParkingSlots[i] = null
                 }
-            })
-        }
-        break;
-        default: {
-            console.log(`Invalid Command`)
-        }
+                console.log(`Created parking lot with ${commandAndArguments[1]} slots`)
+
+            }
 
         }
+        break;
+    case 'park': {
+        const slotNumber = totalParkingSlots.indexOf(null)
+        if (slotNumber !== -1) {
+            totalParkingSlots[slotNumber] = commandAndArguments[1];
+            console.log(`Allocated slot number: ${slotNumber+1}`)
+        } else {
+            console.log('Sorry, parking lot is full')
+        }
+    }
+    break;
+    case 'leave': {
+        const indexOfParkedCar = totalParkingSlots.indexOf(commandAndArguments[1])
+        if (indexOfParkedCar !== -1) {
+            const hours = commandAndArguments[2]
+            const charge = 10 + ((hours - 2) * 10)
+            console.log(`Registration number ${commandAndArguments[1]} with Slot Number ${indexOfParkedCar+1} is free with Charge ${charge}`);
+            totalParkingSlots[indexOfParkedCar] = null
+        } else {
+            console.log(`Registration number ${commandAndArguments[1]} not found`);
+        }
+    }
+    break;
+    case 'status': {
+        console.log(`Slot No.    Registration No.`)
+        totalParkingSlots.forEach((parkedCar, index) => {
+            if (parkedCar) {
+                console.log(`${index+1}           ${parkedCar}`)
+            }
+        })
+    }
+    break;
+    default: {
+        console.log(`Invalid Command`)
+    }
+
+    }
 
     ;
 };
-if(process.argv[2]){const readInterface = readline.createInterface({
-    input: fs.createReadStream(`${process.argv[2]}`),
-    // output: process.stdout,
-    console: false
-});
-readInterface.on('line', function (line){
-    processInputCommand(line)
-})}
+if (process.argv[2]) {
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream(`${process.argv[2]}`),
+        // output: process.stdout,
+        console: false
+    });
+    readInterface.on('line', function (line) {
+        processInputCommand(line)
+    })
+}
 
 module.exports = processInputCommand;
